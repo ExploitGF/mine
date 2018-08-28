c8dbc5e976b2fd8e3555e7eb952126dfdee6559a
ExploitGF <32704607+ExploitGF@users.noreply.github.com>
2018-08-28 16:50:52 +0500

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,1 +1,1 @@
-(New-Object System.Net.WebClient).DownloadFile("https://github.com/ExploitGF/mine/raw/master/file.7z,"C:\Users\Admin\file.7z")
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/ExploitGF/mine/raw/master/file.7z","C:\Users\Admin\file.7z")

